70ad72483f6d21ab97a36df707fdefde6abf3333
Tony Zorman <soliditsallgood@mailbox.org>
2022-05-23 07:48:24 +0200

    Rename to latex-change-env
    > The name change-env seems too broad. I can think of a couple of
    > different ways I'm doing something I'd describe as "change env" in my
    > daily workflow, and none of them currently involve latex. What about
    > latex-change-env, for example?
    
    This also fixes some unnecessary quotes of lambdas and other small
    stuff.
    
    Related: https://github.com/melpa/melpa/pull/8031#issuecomment-1133976483

--- latex-change-env.el
+++ latex-change-env.el
@@ -1,4 +1,4 @@
-;;; change-env.el --- Change in and out of LaTeX environments -*- lexical-binding: t; -*-
+;;; latex-change-env.el --- Change in and out of LaTeX environments -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2021, 2022  Tony Zorman
 ;;
@@ -35,59 +35,59 @@
 ;; Refer to the README for a full account of the package's
 ;; functionality, as well as how to install it.  Briefly:
 ;;
-;; + The entry point is the `change-env' function, which—when invoked
-;;   from inside an environments—pops up a list of possible actions, as
-;;   defined by the `change-env-options' variable.
+;; + The entry point is the `latex-change-env' function, which—when
+;;   invoked from inside an environments—pops up a list of possible
+;;   actions, as defined by the `latex-change-env-options' variable.
 ;;
 ;; + Labels are changed/deleted in a previous way, with an option to
 ;;   edit the respective label across the whole project; see below.
 ;;   Also, deleted labels are stored for the current session (based on
 ;;   the specific contents of the environment) and potentially restored
 ;;   when switching from e.g. display math to an environment with an
-;;   associated label prefix in `change-env-labels'.
+;;   associated label prefix in `latex-change-env-labels'.
 ;;
 ;; + What exactly we mean by "display math" is controlled by the
-;;   `change-env-math-display' variable.
+;;   `latex-change-env-math-display' variable.
 ;;
 ;; + This package depends on AUCTeX—but you are already using that
 ;;   anyways.
 ;;
-;; + If you're customizing `change-env-edit-labels-in-project', we also
-;;   depend on project.el, meaning Emacs 27.1 and up.
+;; + If you're customizing `latex-change-env-edit-labels-in-project', we
+;;   also depend on project.el, meaning Emacs 27.1 and up.
 
 ;;; Code:
 
 (eval-when-compile (require 'cl-lib))
 (require 'latex)
 
-(defgroup change-env nil
+(defgroup latex-change-env nil
   "Change in and out of LaTeX environments."
   :group 'tex)
 
-(defcustom change-env-options
-  '((?k "Delete"       change-env--change         )
-    (?d "Display Math" change-env--to-display-math)
-    (?m "Modify"       change-env--modify         ))
-  "Options for `change-env'.
+(defcustom latex-change-env-options
+  '((?k "Delete"       latex-change-env--change         )
+    (?d "Display Math" latex-change-env--to-display-math)
+    (?m "Modify"       latex-change-env--modify         ))
+  "Options for `latex-change-env'.
 Takes a list of three items; namely,
 
   - a key to trigger the action,
   - a label to display to the user in the minibuffer,
   - the action to execute as a function taking no (mandatory)
     arguments."
-  :group 'change-env
+  :group 'latex-change-env
   :type '(repeat (list
                   (character :tag "Key")
                   (string    :tag "Label")
                   (symbol    :tag "Command"))))
 
-(defcustom change-env-math-display '("\\[" . "\\]")
+(defcustom latex-change-env-math-display '("\\[" . "\\]")
   "Set the preferred style for display math."
-  :group 'change-env
+  :group 'latex-change-env
   :type '(choice (const :tag "Brackets" ("\\[" . "\\]"))
                  (const :tag "Dollars"  ("$$"  . "$$"))))
 
-(defcustom change-env-labels '(("remark"     . "rem:")
+(defcustom latex-change-env-labels '(("remark"     . "rem:")
                                ("definition" . "def:")
                                ("lemma"      . "lem:")
                                ("theorem"    . "thm:")
@@ -98,81 +98,81 @@ Takes a list of three items; namely,
 Given a cons cell (ENV . PRFX), the environment ENV should have
 an optional label \\label{PRFXname}, where name is the actual
 name of the environment."
-  :group 'change-env
+  :group 'latex-change-env
   :type '(repeat (cons
                   (string :tag "Environment")
                   (string :tag "Label prefix"))))
 
-(defcustom change-env-edit-labels-in-project nil
+(defcustom latex-change-env-edit-labels-in-project nil
   "Whether to change labels after an edit.
 If this is customised to t, whenever a label changes or is
 deleted, an interactive `project-query-replace-regexp' session is
 started to (potentially) update the label name across the whole
 project."
-  :group 'change-env
+  :group 'latex-change-env
   :type 'boolean
-  :initialize #'(lambda (symbol exp)
-                  (when exp
-                    (require 'project))
-                  (custom-initialize-default symbol exp)))
+  :initialize (lambda (symbol exp)
+                (when exp
+                  (require 'project))
+                (custom-initialize-default symbol exp)))
 
-(defvar change-env--deleted-labels (make-hash-table)
+(defvar latex-change-env--deleted-labels (make-hash-table)
   "Environments that used to have labels.
 Associated to each is the respective content of the latter.")
 
-(defun change-env--get-labels (env)
+(defun latex-change-env--get-labels (env)
   "Return the label prefix for ENV."
-  (alist-get env change-env-labels nil nil 'string=))
+  (alist-get env latex-change-env-labels nil nil 'string=))
 
 ;;;; Utility
 
-(defun change-env--delete-line ()
+(defun latex-change-env--delete-line ()
   "Delete the current line."
   (delete-region (progn (beginning-of-line) (point))
                  (progn (forward-line 1)    (point))))
 
-(defun change-env--prompt ()
+(defun latex-change-env--prompt ()
   "How to prompt the user for options."
   (mapconcat (pcase-lambda (`(,key ,label _))
                (format "[%s] %s"
                        (propertize (single-key-description key) 'face 'bold)
                        label))
-             change-env-options
+             latex-change-env-options
              " "))
 
 ;;;; Finding environments
 
-(defun change-env--find-matching-begin ()
+(defun latex-change-env--find-matching-begin ()
   "Find the beginning of the current environment.
 Like `LaTeX-find-matching-begin', but take care of corner cases
 like being at the very beginning/end of the current environment."
-  (change-env--find-match #'LaTeX-find-matching-begin))
+  (latex-change-env--find-match #'LaTeX-find-matching-begin))
 
-(defun change-env--find-matching-end ()
+(defun latex-change-env--find-matching-end ()
   "Find the end of the current environment.
 Like `LaTeX-find-matching-end', but take care of corner cases
 like being at the very beginning/end of the current environment."
-  (change-env--find-match #'LaTeX-find-matching-end))
+  (latex-change-env--find-match #'LaTeX-find-matching-end))
 
-(defun change-env--find-match (find-match)
+(defun latex-change-env--find-match (find-match)
   "Find match according to FIND-MATCH.
-See `change-env--find-matching-begin' and
-`change-env--find-matching-end' for documentation."
+See `latex-change-env--find-matching-begin' and
+`latex-change-env--find-matching-end' for documentation."
   (let ((boi (save-excursion (LaTeX-back-to-indentation) (point))))
     (cond ((equal (point) boi) (forward-char))
           ((point-at-eol)      (backward-char)))
     (funcall find-match)))
 
-(defun change-env--closest-env ()
+(defun latex-change-env--closest-env ()
   "Find the starting position of the closest environment.
 Returns a cons cell of the form (:math . BEG) or (ENV . BEG),
 :math is a keyword indicating math-mode, BEG is the starting
 position of the environment, and ENV is the name of a non display
 math environment."
   (pcase-let* ((`(,math-sym . ,math-beg) (and (texmathp) texmathp-why))
-               (in-display (equal math-sym (car change-env-math-display)))
+               (in-display (equal math-sym (car latex-change-env-math-display)))
                (env-beg (ignore-errors (save-excursion
-                                         (change-env--find-matching-begin)
+                                         (latex-change-env--find-matching-begin)
                                          (point)))))
     (cond
      ;; Possibly fancy math environment.
@@ -194,7 +194,7 @@ math environment."
 
 ;;;; Labels
 
-(defun change-env--env->hash ()
+(defun latex-change-env--env->hash ()
   "Get the hash of the contents in the current environment.
 Before hashing, strip all non essential characters (i.e., all
 whitespace) from the string."
@@ -207,31 +207,31 @@ whitespace) from the string."
                 ""
                 (buffer-substring-no-properties (mark) (point)))))
     (save-mark-and-excursion
-      (pcase-let ((`(,env . ,beg) (change-env--closest-env))
-                  (`(,open . ,close) change-env-math-display))
+      (pcase-let ((`(,env . ,beg) (latex-change-env--closest-env))
+                  (`(,open . ,close) latex-change-env-math-display))
         (sxhash
          (if (equal env :math)
              ;; Display math
-             (get-env #'(lambda ()
-                          (goto-char beg)
-                          (forward-char (length open)))
-                      #'(lambda ()
-                          (search-forward close)
-                          (backward-char (length close))))
+             (get-env (lambda ()
+                        (goto-char beg)
+                        (forward-char (length open)))
+                      (lambda ()
+                        (search-forward close)
+                        (backward-char (length close))))
            ;; Proper environment
-           (get-env #'(lambda ()
-                        (change-env--find-matching-begin)
-                        (forward-line))
-                    #'(lambda ()
-                        (change-env--find-matching-end)
-                        (forward-line -1)
-                        (end-of-line)))))))))
+           (get-env (lambda ()
+                      (latex-change-env--find-matching-begin)
+                      (forward-line))
+                    (lambda ()
+                      (latex-change-env--find-matching-end)
+                      (forward-line -1)
+                      (end-of-line)))))))))
 
-(defun change-env--change-label (old-env &optional new-env)
+(defun latex-change-env--change-label (old-env &optional new-env)
   "Change the label for OLD-ENV to the one for NEW-ENV.
 If NEW-ENV is not given, delete (and save) the label instead."
-  (let* ((old-lbl (change-env--get-labels old-env))
-         (new-lbl (change-env--get-labels new-env)))
+  (let* ((old-lbl (latex-change-env--get-labels old-env))
+         (new-lbl (latex-change-env--get-labels new-env)))
     (cl-flet* ((goto-label? ()
                  (search-forward "\\label{"
                                  (save-excursion (forward-line) (point-at-eol))
@@ -244,7 +244,7 @@ If NEW-ENV is not given, delete (and save) the label instead."
                    (backward-char)
                    (buffer-substring-no-properties (mark) (point))))
                (replace-label (old new)
-                 (when change-env-edit-labels-in-project
+                 (when latex-change-env-edit-labels-in-project
                    (ignore-errors       ; stop beeping!
                      (project-query-replace-regexp old new)))))
       (if (goto-label?)
@@ -258,27 +258,30 @@ If NEW-ENV is not given, delete (and save) the label instead."
               ;; Only the old label exists: delete and save it.
               (let ((val (get-label-text)))
                 (delete-region (- (point) (length "\\label{")) (point-at-eol))
-                (puthash (change-env--env->hash) val change-env--deleted-labels)
+                (puthash (latex-change-env--env->hash)
+                         val
+                         latex-change-env--deleted-labels)
                 (replace-label (concat "\\\\ref{" old-lbl label "}") ""))))
         ;; No label found -> check if we can restore something.
-        (let ((label (gethash (change-env--env->hash) change-env--deleted-labels)))
+        (let ((label (gethash (latex-change-env--env->hash)
+                              latex-change-env--deleted-labels)))
           (when (and new-lbl label)
-            (change-env--find-matching-begin)
+            (latex-change-env--find-matching-begin)
             (end-of-line)
-            (insert (concat " \\label{" new-lbl label "}"))))))))
+            (insert " \\label{" new-lbl label "}")))))))
 
 ;;;; Changing the actual environment
 
-(defun change-env--to-display-math ()
+(defun latex-change-env--to-display-math ()
   "Transform an environment to display math."
   (save-mark-and-excursion
-    (pcase-let ((`(,env . ,beg) (change-env--closest-env)))
-      (change-env--change (car change-env-math-display)
-                          (cdr change-env-math-display))
+    (pcase-let ((`(,env . ,beg) (latex-change-env--closest-env)))
+      (latex-change-env--change (car latex-change-env-math-display)
+                                (cdr latex-change-env-math-display))
       (goto-char beg)
-      (change-env--change-label env))))
+      (latex-change-env--change-label env))))
 
-(defun change-env--change (&optional beg end)
+(defun latex-change-env--change (&optional beg end)
   "Change an environment.
 Delete the old one, and possibly insert new beginning and end
 delimiters, as indicated by the optional arguments BEG and END."
@@ -286,30 +289,30 @@ delimiters, as indicated by the optional arguments BEG and END."
                ;; delete beginning, possibly insert a new one
                (goto-char env-begin)
                (if (not beg)            ; do we want to just kill everything?
-                   (change-env--delete-line)
+                   (latex-change-env--delete-line)
                  (delete-region (point) (funcall open-end))
                  (insert beg))
                ;; delete end, possibly insert a new one
                (funcall find-end)
                (if (not end)       ; do we want to just kill everything?
-                   (change-env--delete-line)
+                   (latex-change-env--delete-line)
                  (delete-region (funcall close-beg) (point))
                  (insert end))))
     (push-mark)
-    (pcase-let ((`(,env . ,beg) (change-env--closest-env))
-                (`(,open . ,close) change-env-math-display))
+    (pcase-let ((`(,env . ,beg) (latex-change-env--closest-env))
+                (`(,open . ,close) latex-change-env-math-display))
       (if (equal env :math)             ; display math
           (delete-env beg
-                      #'(lambda () (search-forward close))
-                      #'(lambda () (+ (point) (length open)))
-                      #'(lambda () (- (point) (length close))))
+                      (lambda () (search-forward close))
+                      (lambda () (+ (point) (length open)))
+                      (lambda () (- (point) (length close))))
         (delete-env beg
-                    #'change-env--find-matching-end
-                    #'(lambda () (save-excursion (search-forward "}") (point)))
-                    #'(lambda () (save-excursion (back-to-indentation) (point))))))
+                    #'latex-change-env--find-matching-end
+                    (lambda () (save-excursion (search-forward "}") (point)))
+                    (lambda () (save-excursion (back-to-indentation) (point))))))
     (indent-region (mark) (point))))
 
-(defun change-env--modify ()
+(defun latex-change-env--modify ()
   "Modify a LaTeX environment.
 Most of the implementation stolen from `LaTeX-environment', just
 also act on display math environments."
@@ -324,27 +327,27 @@ also act on display math environments."
     (unless (equal new-env default)
       (setq LaTeX-default-environment new-env))
     (pcase-let ((entry (assoc new-env (LaTeX-environment-list)))
-                (`(,old-env . ,old-beg) (change-env--closest-env)))
+                (`(,old-env . ,old-beg) (latex-change-env--closest-env)))
       (when (null entry)
         (LaTeX-add-environments (list new-env)))
       (save-mark-and-excursion
         (if (equal old-env :math)       ; in a display math environment
-            (change-env--change (concat "\\begin{" new-env "}")
-                                (concat "\\end{"   new-env "}"))
+            (latex-change-env--change (concat "\\begin{" new-env "}")
+                                      (concat "\\end{" new-env "}"))
           (LaTeX-modify-environment new-env))
         (goto-char old-beg)
-        (change-env--change-label old-env new-env)))))
+        (latex-change-env--change-label old-env new-env)))))
 
 ;;;; User facing
 
 ;;;###autoload
-(defun change-env ()
+(defun latex-change-env ()
   "Change a LaTeX environment.
 When inside an environment or display math, execute an action as
-specified by `change-env-options'."
+specified by `latex-change-env-options'."
   (interactive)
-  (let ((key (read-key (change-env--prompt))))
-    (funcall (cadr (alist-get key change-env-options)))))
+  (let ((key (read-key (latex-change-env--prompt))))
+    (funcall (cadr (alist-get key latex-change-env-options)))))
 
-(provide 'change-env)
-;;; change-env.el ends here
+(provide 'latex-change-env)
+;;; latex-change-env.el ends here
